43392f76f895ec49e4d0090d8bb3e6b1bdeff693
Lauan Amorim <lauan12x@gmail.com>
2023-05-22 10:40:17 -0300

    Exe 6, 22/05

---
+++ Areas/Veiculo/Controllers/AutoController.cs
@@ -0,0 +1,37 @@
+﻿using ExeSeneLauIII.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeSeneLauIII.Areas.Veiculo.Controllers
+{
+    public class AutoController : Controller
+    {
+        // GET: Veiculo/Auto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConsultaVeiculo()
+        {
+
+            return View();
+        }
+       public ActionResult ConsultaCarro() 
+       {
+        
+            return View();
+       }
+    
+        public ActionResult ConsultaMoto() 
+        {
+            Moto moto = new Moto();
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExeSeneLauIII.MvcApplication" Language="C#" %>
